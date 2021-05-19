99ed51e505037735e6c9046f22df5ce7bd57f7bc
Megiddo <megiddo.lions@gmail.com>
2021-05-19 17:58:55 +0300

    blue Tower vision

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vison/pipelines/align/BlueTowerAlignPipeLine.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vison/pipelines/align/BlueTowerAlignPipeLine.java
@@ -1,9 +1,7 @@
 package org.firstinspires.ftc.teamcode.vison.pipelines.align;
 
-import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
-import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
@@ -11,21 +9,18 @@ import org.opencv.imgproc.Imgproc;
 import java.util.ArrayList;
 
 import static org.firstinspires.ftc.teamcode.Constants.VisionConstants.camera_width;
-import static org.firstinspires.ftc.teamcode.Constants.VisionConstants.camera_height;
-import static org.firstinspires.ftc.teamcode.lib.CvUtil.cvErode;
 import static org.firstinspires.ftc.teamcode.lib.CvUtil.filterContours;
 import static org.firstinspires.ftc.teamcode.lib.CvUtil.findContours;
-import static org.firstinspires.ftc.teamcode.lib.CvUtil.mask;
 
 public class BlueTowerAlignPipeLine extends AlignPipeLine {
     private final static Rect view_rect = new Rect(0, 0, camera_width, 400);
 
-    public final static double filterContoursMinArea = 400;
+    public final static double filterContoursMinArea = 600;
     public final static double filterContoursMinPerimeter = 0;
     public final static double filterContoursMinWidth = 0;
-    public final static double filterContoursMaxWidth = 0;
-    public final static double filterContoursMinHeight = 200;
-    public final static double filterContoursMaxHeight = 1000;
+    public final static double filterContoursMaxWidth = 30;
+    public final static double filterContoursMinHeight = 0;
+    public final static double filterContoursMaxHeight = 200;
     public final static double[] filterContoursSolidity = {0, 100};
     public final static double filterContoursMaxVertices = 1000000;
     public final static double filterContoursMinVertices = 0;
